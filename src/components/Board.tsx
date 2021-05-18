839d7d0d331011d11f4a5183fd6b75c446ca2feb
forest1040 <forest1040@gmail.com>
2021-05-18 22:10:04 +0900

    serviceを追加

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -11,6 +11,7 @@ import MessagePreview from './CardEdit';
 import { FILE_EVENTS, saveFile, FileInfoType } from '../fileIO';
 import { textState, fileNameState } from '../models/State';
 import { SPLIT_MESSAGE } from '../models/Const';
+import { createCards } from '../services/Card';
 
 const openFileDialog = (): void => {
   ipcRenderer.send(FILE_EVENTS.OPEN_DIALOG);
@@ -20,63 +21,8 @@ const openSaveAsDialog = (fileInfo: FileInfoType): void => {
   ipcRenderer.send(FILE_EVENTS.SAVE_DIALOG, fileInfo);
 };
 
-async function createCards(text: string) {
-  // // 先頭のSPLIT_MESSAGEを削除
-  // const re = new RegExp('^' + SPLIT_MESSAGE);
-  // let tmp = text.replace(re, '').trim();
-  const tmp = text.replaceAll('\r\n', '\n');
-  const msgs = tmp.split(SPLIT_MESSAGE + '\n');
-  //const msgs = text.split(/\r\n|\n/);
-  const now = new Date();
-  const cards = msgs
-    .map((m) => {
-      // const lines = m.split(/\r\n|\n/);
-      const lines = m.split('\n');
-      //const check = lines.map((l) => l.trim());
-
-      if (lines.length > 0) {
-        // 空の場合はスキップ
-        if (lines[0].trim().length == 0) return undefined;
-        // 1行目の # を探す
-        const head = lines[0];
-        let title = '';
-        let id = 0;
-        if (head.length > 0 && head[0] == '#') {
-          // idを探す
-          const m = head.match(/\(id:\d+\)/g);
-          if (m != null) {
-            // idがある場合
-            const tmp = m[0].match(/\d+/g) || ['0'];
-            id = parseInt(tmp[0]);
-            const idPos = head.indexOf('(id:');
-            title = head.substring(1, idPos).trim();
-          } else {
-            // titleのみ 「#」を除くため、substring(1)してtrimする
-            title = head.substring(1).trim();
-          }
-        }
-
-        if (id == 0) {
-          // 新規
-          return {
-            title: title,
-            text: title.length > 0 ? lines.slice(1).join('\n') : m,
-            createdDate: now,
-            updatedDate: now,
-          };
-        } else {
-          // 更新の場合
-          return {
-            id: id,
-            title: title,
-            text: lines.slice(1).join('\n'),
-            updatedDate: now,
-          };
-        }
-      }
-      return undefined;
-    })
-    .filter((x) => x);
+async function _createCards(text: string) {
+  const cards = createCards(text);
   if (cards.length > 0) {
     return await ipcRenderer.invoke('create-cards', cards);
   }
@@ -155,7 +101,7 @@ ${card.text}${SPLIT_MESSAGE}
 
   const handleDBSave = useCallback(() => {
     //createCards(text);
-    createCards(text).then((cards: Card[]) => {
+    _createCards(text).then((cards: Card[]) => {
       setText(
         cards
           .map((m) => {
---
+++ src/services/Card.ts
@@ -0,0 +1,62 @@
+import { SPLIT_MESSAGE } from '../models/Const';
+
+export function createCards(text: string) {
+  // // 先頭のSPLIT_MESSAGEを削除
+  // const re = new RegExp('^' + SPLIT_MESSAGE);
+  // let tmp = text.replace(re, '').trim();
+  const tmp = text.replaceAll('\r\n', '\n');
+  const msgs = tmp.split(SPLIT_MESSAGE + '\n');
+  //const msgs = text.split(/\r\n|\n/);
+  const now = new Date();
+  const cards = msgs
+    .map((m) => {
+      // const lines = m.split(/\r\n|\n/);
+      const lines = m.split('\n');
+      //const check = lines.map((l) => l.trim());
+
+      if (lines.length > 0) {
+        // 空の場合はスキップ
+        if (lines[0].trim().length == 0) return undefined;
+        // 1行目の # を探す
+        const head = lines[0];
+        let title = '';
+        let id = 0;
+        if (head.length > 0 && head[0] == '#') {
+          // idを探す
+          const m = head.match(/\(id:\d+\)/g);
+          if (m != null) {
+            // idがある場合
+            const tmp = m[0].match(/\d+/g) || ['0'];
+            id = parseInt(tmp[0]);
+            const idPos = head.indexOf('(id:');
+            title = head.substring(1, idPos).trim();
+          } else {
+            // titleのみ 「#」を除くため、substring(1)してtrimする
+            title = head.substring(1).trim();
+          }
+        }
+
+        if (id == 0) {
+          // 新規
+          return {
+            title: title,
+            text: title.length > 0 ? lines.slice(1).join('\n') : m,
+            createdDate: now,
+            updatedDate: now,
+          };
+        } else {
+          // 更新の場合
+          return {
+            id: id,
+            title: title,
+            text: lines.slice(1).join('\n'),
+            updatedDate: now,
+          };
+        }
+      }
+      return undefined;
+    })
+    .filter((x) => x);
+
+  return cards;
+}
